50e702d5cfad180f1cb7f11cb0fa358d341519e6
ayca <gurleyikayca@gmail.com>
2020-02-07 00:42:21 +0300

    Mid Parking Lot with Unity Project
    Modeled Mid Parking Lot basically in 3D using Maya.  Transferred to Unity as an asset.  Script code was written to get requests and marking available parking slots with 3D marker from the status in the request. Added screen touch script for smartphone.

---
+++ MID_OTOPARK/Assets/requestScript.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+public class requestScript : MonoBehaviour{
+
+public GameObject spawnee;
+public Transform pos;
+
+    public void Start(){
+        StartCoroutine(coroutine());
+    }
+    
+    public IEnumerator coroutine(){
+        while (true){
+            //UnityWebRequest www = UnityWebRequest.Get("http://localhost:3000/parkinglots");
+            UnityWebRequest www = UnityWebRequest.Get("http://localhost:3000/parkinglots/P1");
+
+            yield return www.SendWebRequest();
+            if (www.isNetworkError || www.isHttpError){
+                Debug.Log(www.error);
+                Debug.Log("ERROR");
+            }
+            
+            else{
+              
+                // Show results as text
+                string str = www.downloadHandler.text;
+                Debug.Log(www.downloadHandler.text);
+            
+                int status = 1;
+                //bu statusu databaseten alınca bu kismi guncelle
+                
+                if(status == 1){
+                    Instantiate(spawnee,pos.position + new Vector3(0,(float)0.40,0),pos.rotation);
+                                
+                }          
+            }
+            
+            yield return new WaitForSeconds(5);
+        }
+    }
+}
