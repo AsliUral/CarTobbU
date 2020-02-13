82413bde3246e56f13bdd15d1b33f0e57e8cb5cc
unibodydesignn <unibodydesignn@gmail.com>
2020-02-13 23:45:23 +0300

    exit enter animations

---
+++ TM Otopark/Assets/carExitTrigger.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class carExitTrigger : MonoBehaviour
+{
+    public Animator carExitAnim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        carExitAnim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown("1"))
+        {
+            carExitAnim.Play("carExit");
+        }
+    }
+}
