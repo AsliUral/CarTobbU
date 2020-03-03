4d8720f04adb536c77af5441ce512f7f6f351ea7
akyurektuna <43556281+akyurektuna@users.noreply.github.com>
2020-03-03 20:08:50 +0300

    Adding mark action for YDB parking lot
    Users can mark the parking lot that their car is in.  Using unity physics raycast for selecting parking lot that has touch input. TouchPosition.cs script takes touch input and creates a mark object at the touched position.

---
+++ YDB_otopark/Assets/TouchPosition.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UIElements;
+
+public class TouchPosition : MonoBehaviour
+{
+ 
+    private Transform pos;
+    public GameObject mark;
+   public GameObject obj;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0)){
+            Destroy(obj);
+            var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hitInfo;
+            if(Physics.Raycast(ray, out hitInfo)){
+                var rig = hitInfo.collider.GetComponent<Rigidbody>();
+                pos = hitInfo.collider.gameObject.transform;
+                if(pos != null){
+                    obj = Instantiate(mark,pos.position + new Vector3(0,(float)0.60,0),pos.rotation);
+                    obj.transform.Rotate(90,90,0);
+                }
+
+            }
+            
+        }
+        
+    }
+}
