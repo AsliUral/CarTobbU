28f13feb7e17db2a6d091d58ec3d5eb5e9617398
akyurektuna <43556281+akyurektuna@users.noreply.github.com>
2020-03-07 00:47:32 +0300

    New scenes for menu are created
    Main menu, Login and Register scenes are created, scripts for button actions and input functionality will be filled.

--- YDB_otopark/Assets/TouchPosition.cs
+++ YDB_otopark/Assets/TouchPosition.cs
@@ -7,21 +7,22 @@ public class TouchPosition : MonoBehaviour
  
     private Transform pos;
     public GameObject mark;
-   public GameObject obj;
+    private GameObject obj;
 
     // Update is called once per frame
     void Update()
     {
         if(Input.GetMouseButtonDown(0)){
-            Destroy(obj);
+            //Destroy(obj);
             var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             RaycastHit hitInfo;
             if(Physics.Raycast(ray, out hitInfo)){
+                Destroy(obj);
                 var rig = hitInfo.collider.GetComponent<Rigidbody>();
                 pos = hitInfo.collider.gameObject.transform;
                 if(pos != null){
                     obj = Instantiate(mark,pos.position + new Vector3(0,(float)0.60,0),pos.rotation);
-                    obj.transform.Rotate(90,90,0);
+                    obj.transform.Rotate(90,0,0);
                 }
 
             }
