57d11ed7db1121fcb90ebc84809d8ab180ae70a8
AsliUral <aasliuural@gmail.com>
2020-03-06 19:08:38 +0300

    #13 Send to database createParkingLot for marked parking zone with Parking zone number
    If a user marked a parking lot and after the marking any parkinglots are deleted. The marking continues from the latest number.

--- parking-lot-detection/parking_lot_manage_mark/main.py
+++ parking-lot-detection/parking_lot_manage_mark/main.py
@@ -2,22 +2,55 @@ import cv2
 import numpy
 from parking_lot_manage_mark.parking_lot import ParkingLot
 from tobb_etu_smart_car_park_python_api import smart_car_park_python_api
+from parking_lot_detection.ParkingLot.parkingLotGenerator import generateParkingLots
+
+
+
+def getPoint(point):
+    if point is not None:
+        point = point[1:len(point) - 1]
+        point = point.split(',')
+        return [int(point[0]) , int(point[1])]
+
+def get_parking_lots(API):
+    parking_lot_JSON = API.getAllParkingLotsOFParkZone()
+    parkingLots = []
+    counter = 1
+    max = 0
+    for i in range(len(parking_lot_JSON)):
+        if (parking_lot_JSON[i]["FirstPoint"] is not None and parking_lot_JSON[i]["FirstPoint"] != ''):
+            parkingLots.append(ParkingLot(
+                                          from_server=True,
+                                          parkingLotID=parking_lot_JSON[i]["ParkingLotID"],
+                                          pt1=(getPoint(parking_lot_JSON[i]["FirstPoint"])[0],getPoint(parking_lot_JSON[i]["FirstPoint"])[1]),
+                                          pt2=(getPoint(parking_lot_JSON[i]["SecondPoint"])[0],getPoint(parking_lot_JSON[i]["SecondPoint"])[1]),
+                                          pt3=(getPoint(parking_lot_JSON[i]["ThirdPoint"])[0],getPoint(parking_lot_JSON[i]["ThirdPoint"])[1]),
+                                          pt4=(getPoint(parking_lot_JSON[i]["FourthPoint"])[0],getPoint(parking_lot_JSON[i]["FourthPoint"])[1]),
+                                          API=API,
+                                          parkingZone=parking_lot_JSON[i]["ParkZoneName"]))
+            counter += 1
+            id = parking_lot_JSON[i]["ParkingLotID"]
+            if (int(id[1:]) > max):
+                max = int(id[1:])
+
+    print("Return : " , parkingLots)
+    return parkingLots, max + 1
+
 
 COLOR_WHITE = (255, 255, 255)
 cameraID = "1"
-parkZoneID = "1"
+parkZoneID = "5"
 apiKey = "ABC"
 camera = 'video/parking_lot_1.mp4'
 
-cap = cv2.VideoCapture('video/parking_lot_1.mp4')
-if cap.isOpened() == False:
-    print(
-        "Error opening the video file. Please double check your file path for typos. Or move the movie file to the same location as this script/notebook")
-parking_lots = []
+
+API = smart_car_park_python_api.SmartCarParkAPI(cameraID, parkZoneID, apiKey)
+parking_lots, tempID  = get_parking_lots(API)
 ids = 0
 a = 1
+startDetection = False
 currentlyMarked = False
-API = smart_car_park_python_api.SmartCarParkAPI(cameraID, parkZoneID, apiKey)
+
 
 
 
@@ -62,8 +95,6 @@ botRight_clicked = False
 botLeft_clicked = False
 topRight_clicked = False
 
-tempID = 1
-
 coordinates = []
 
 cv2.namedWindow('Tobb Etu Car Park')
@@ -72,6 +103,10 @@ cv2.setMouseCallback('Tobb Etu Car Park', draw_polygon)
 
 
 
+cap = cv2.VideoCapture('video/parking_lot_1.mp4')
+if cap.isOpened() == False:
+    print(
+        "Error opening the video file. Please double check your file path for typos. Or move the movie file to the same location as this script/notebook")
 while True:
     ret, frame = cap.read()
     for parking_lot in parking_lots:
@@ -81,7 +116,7 @@ while True:
 
 
     if topLeft_clicked and botRight_clicked and topRight_clicked and botLeft_clicked and not currentlyMarked:
-        parking_lot = ParkingLot(pt1, pt2, pt3, pt4, "P" +str(tempID), "MainCarPark",API)
+        parking_lot = ParkingLot(pt1, pt2, pt3, pt4, "C" +str(tempID), "Main Car Park",API,from_server=False)
         parking_lots.append(parking_lot)
         parking_lot.draw_parking_lot(frame)
         tempID = tempID + 1
@@ -97,6 +132,12 @@ while True:
     if cv2.waitKey(1) & 0xFF == ord('r'):
         frame = frame.copy()
 
+    if cv2.waitKey(1) & 0xFF == ord('o'):
+        startDetection = True
+
+
 
 cap.release()
 cv2.destroyAllWindows()
+
+
--- parking-lot-detection/parking_lot_manage_mark/parking_lot.py
+++ parking-lot-detection/parking_lot_manage_mark/parking_lot.py
@@ -3,17 +3,24 @@ import cv2
 
 class ParkingLot:
 
-  def __init__(self, pt1, pt2, pt3, pt4, parkingLotID, parkingZone,API):
+  def __init__(self, pt1, pt2, pt3, pt4, parkingLotID, parkingZone,API, from_server):
     pts = numpy.array([list(pt1), list(pt2), list(pt3), list(pt4)], numpy.int32)
     pts = pts.reshape((-1, 1, 2))
     self.points = pts
     self.parkingLotID = parkingLotID
     self.parkingZone = parkingZone
     self.API = API
+    self.from_server = from_server
+    first = "[" + str(pt1[0]) + "," + str(pt1[1]) + "]"
+    second = "[" + str(pt2[0]) + "," + str(pt2[1]) + "]"
+    third = "[" + str(pt3[0]) + "," + str(pt3[1]) + "]"
+    fourth = "[" + str(pt4[0]) + "," + str(pt4[1]) + "]"
+    if (from_server == False):
+        self.API.createParkingLot("Available", self.parkingLotID, parkingZone, first, second, third, fourth)
+
 
   def draw_parking_lot(self, frame):
     cv2.polylines(frame, [self.points], True, (51, 255, 255), 2)
-    self.API.createParkingLot("Available",self.parkingLotID,"MainCarPark",self.points[0],self.points[1],self.points[2],self.points[3])
 
 
   def draw_contours(self, frame):
