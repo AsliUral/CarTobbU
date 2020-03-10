7f37a089676f4d66e03ec8816aa223ee9dc781d7
ayca <gurleyikayca@gmail.com>
2020-03-10 18:38:18 +0300

    #17 Getting the status of parking lots from the web server and adding new assets for MID OTOPARK
    -Getting parking lots status from the server, handling requests and showing them status availabe or occupied in Unity
    -Adding new assets for designing Mid Car Park Area

--- MID_OTOPARK/Assets/requestScript.cs
+++ MID_OTOPARK/Assets/requestScript.cs
@@ -2,9 +2,10 @@
 using System.Collections;
 using UnityEngine.Networking;
 public class requestScript : MonoBehaviour{
-
 public GameObject spawnee;
+public GameObject car;
 public Transform pos;
+public Transform transform;
 
     public void Start(){
         StartCoroutine(coroutine());
@@ -12,36 +13,53 @@ public Transform pos;
     
     public IEnumerator coroutine(){
         while (true){
-            //UnityWebRequest www = UnityWebRequest.Get("http://localhost:3000/parkinglots");
-            UnityWebRequest www = UnityWebRequest.Get("http://localhost:3000/parkinglots/P1");
+            
+            //UnityWebRequest www = UnityWebRequest.Get("https://smart-car-park-api.appspot.com/parkingLots/p11");
+            UnityWebRequest www = UnityWebRequest.Get("https://smart-car-park-api.appspot.com/parkingLots/p2");
 
             yield return www.SendWebRequest();
+           
             if (www.isNetworkError || www.isHttpError){
                 Debug.Log(www.error);
-                Debug.Log("ERROR");
+                Debug.Log("ERROR!");
             }
             
             else{
-              
+               
                 // Show results as text
-                string str = www.downloadHandler.text;
+                string available ="\"Available\"";
+                string occupied = "\"Occupied\"";
+                
+                string data = www.downloadHandler.text;
                 Debug.Log(www.downloadHandler.text);
-            
-                int status = 1;
-              
-                //bu statusu databaseten alınca bu kismi guncelle
-                
-                if(status == 1){
-                    Instantiate(spawnee,pos.position + new Vector3(0,(float)0.40,0),pos.rotation);
-                              
+                string[] allStatus;
+                string parkingLotStatus = "";
+                string[] dataSplit = data.Split(',');
+                for(var i=0; i<dataSplit.Length; i++){
+                    string parkId=dataSplit[0];
+                    if(dataSplit[i].Contains("ParkingLotStatus")){
+                        allStatus = dataSplit[i].Split(':');
+                        parkingLotStatus = allStatus[1];
+                        Debug.Log("parking lot status of "+ parkId+" -> " + parkingLotStatus);
+                    }
+               
                 }
-     
-                
-             
+                var cubeRenderer = spawnee.GetComponent<Renderer>();
+                var cubeRenderer2 = car.GetComponent<Renderer>();
+                if(parkingLotStatus.Equals(available)){
+
+                    Debug.Log("available");
+                    Instantiate(spawnee,pos.position + new Vector3(0,(float)0.40,0),pos.rotation);                 
+                }else if(parkingLotStatus.Equals(occupied)){
+                    Debug.Log("occupied");
+                    Instantiate(car,pos.position + new Vector3(0,(float)0,0),pos.rotation);
+                    car.transform.Rotate(0,-90,0); 
+                    
                 }          
             }
             
             yield return new WaitForSeconds(5);
         }
     }
-
+   
+}
