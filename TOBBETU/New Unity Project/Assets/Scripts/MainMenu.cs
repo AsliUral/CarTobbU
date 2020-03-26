09186120a7beb524ee48c1969f59b1ca0d6f23f3
akyurektuna <43556281+akyurektuna@users.noreply.github.com>
2020-03-27 00:05:35 +0300

    adding MainMenu and Registration scripts

---
+++ TOBBETU/New Unity Project/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+   public void GoToRegister(){
+       SceneManager.LoadScene(2);
+       /*
+       scene 0 = parkinglot scene
+       scene 1 = main menu scene
+       scene 2 = register scene
+       scene 3 = login scene
+   
+        */
+   }
+
+   public void GoToLogin(){
+      SceneManager.LoadScene(3);
+   }
+}
