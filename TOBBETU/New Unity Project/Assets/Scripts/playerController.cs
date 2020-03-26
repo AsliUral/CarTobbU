8dad7ae1acf3df2afb014b7f204943a352735ce1
ayca <gurleyikayca@gmail.com>
2020-03-27 00:39:02 +0300

    Adding NavMeshComponets and playerControl script for using to navigate user in the Unity app
    In the Unity app user as a player if click somewhere to reach then user can react to destination with navigation path and just walk to walkable areas.

---
+++ TOBBETU/New Unity Project/Assets/Scripts/playerController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class playerController : MonoBehaviour
+{
+    public Camera cam;
+    public NavMeshAgent agent;
+
+    // Start is called before the first frame update
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0)){
+            
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if(Physics.Raycast(ray , out hit)){
+                agent.SetDestination(hit.point);
+            
+            }
+        }
+        
+    }
+}
