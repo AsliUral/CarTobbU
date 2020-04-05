dacdfcd9072f287302e7fd82cbc12f978c48f508
ayca <gurleyikayca@gmail.com>
2020-04-05 22:29:25 +0300

    #24 Display the system time.
    -User can see the time while using the mobile application.

---
+++ TOBBETU/New Unity Project/Assets/Scripts/DisplayTime.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class DisplayTime : MonoBehaviour
+{
+    public GameObject theDisplay;
+    public int seconds;
+    public int hour;
+    public int minutes;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        hour=System.DateTime.Now.Hour;
+        seconds=System.DateTime.Now.Second;
+        minutes=System.DateTime.Now.Minute;
+        theDisplay.GetComponent<Text>().text="   Hi!"+"\n"+hour+":"+minutes+":"+seconds;
+        
+    }
+}
