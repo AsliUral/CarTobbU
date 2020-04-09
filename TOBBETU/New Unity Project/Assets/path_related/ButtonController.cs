6c391c27155e68c29b66bf4a369856676f484e72
unibodydesignn <unibodydesignn@gmail.com>
2020-04-09 20:21:34 +0300

    UI functionality controller

---
+++ TOBBETU/New Unity Project/Assets/path_related/ButtonController.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonController : MonoBehaviour
+{
+
+    PathFinder finder;
+    GameObject agent;
+    GameObject text;
+
+    bool selectedButtonClicked = false;
+    bool shortestButtonClicked = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        text = GameObject.FindGameObjectWithTag("DialogText");
+        
+        text.SetActive(false);
+        agent = GameObject.FindGameObjectWithTag("PathAgent");
+        if (agent != null)
+        {
+            finder = agent.GetComponent<PathFinder>();
+            if (finder == null)
+            {
+                Debug.Log("Could not initialize component!");
+            }
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void InitializeSelectedClick()
+    {
+        if (selectedButtonClicked == true)
+        {
+            selectedButtonClicked = false;
+            text.SetActive(false);
+        }
+        else
+        {
+            selectedButtonClicked = true;
+            text.SetActive(true);
+            finder.InitializeSelectedClick();
+            finder.MoveOnSelectedClick();
+            Debug.Log("Equalized true");
+            
+        }
+
+    }
+
+
+}
